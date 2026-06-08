e75dea5ea80fa4c2a5e88a569139cf6db2f5edf3
Johansell Villalobos <johansellvilla@gmail.com>
2026-06-08 17:57:13 -0600

    changed to node 24 for building; added base site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig({
             rehypePlugins: [[rehypeKatex, { output: 'html' }]],
         }),
     },
-    site: "https://jkhansell.github.io/portfolio/",
+    site: "https://jkhansell.github.io/portfolio_astro/",
 });
