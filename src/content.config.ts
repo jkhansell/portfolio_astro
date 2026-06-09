c72976d50d7bcbaa4c4b4694d8732cd176922121
Johansell Villalobos <johansellvilla@gmail.com>
2026-06-08 21:23:52 -0600

    added publication collection instead of reading directly; used Astro v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@
 import { defineCollection } from 'astro:content';
 
 // 2. Import loader(s)
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 
 // 3. Import Zod
 import { z } from 'astro/zod';
@@ -16,11 +16,30 @@ const projects = defineCollection({
         tags: z.array(z.string()),
         category: z.string(),
         slug: z.string(),
+        featured: z.boolean(),
         github: z.url().optional(),
         demo: z.url().optional(),
         paper: z.url().optional(),
     }),
 });
 
+// 4. Define a `loader` and `schema` for each collection
+const publications = defineCollection({
+    loader: file('./src/content/publications.json'),
+    schema: z.object({
+        title: z.string(),
+        authors: z.string(),
+        venue: z.string(),
+        year: z.number(),
+        featured: z.boolean(),
+        type: z.string(),
+        bibtex: z.string(),
+        pdf: z.url().optional(),
+        doi: z.string().optional(),
+        abstract: z.string().optional(),
+    }),
+});
+
+
 // 5. Export a single `collections` object to register your collection(s)
-export const collections = { projects };
+export const collections = { projects, publications };
